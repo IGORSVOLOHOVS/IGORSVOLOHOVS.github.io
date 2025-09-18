4acdefe88dcebf3a0d41641b123f96bbb0892d49
IGORSVOLOHOVS <igorsvolohovs@gmail.com>
2025-09-18 10:07:27 +0300

    update js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-    // --- SCROLL ANIMATION LOGIC ---
+document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('.fade-in-section');
 
-    const observer = new IntersectionObserver((entries) => {
+    const observer = new IntersectionObserver((entries, observer) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('is-visible');
+                observer.unobserve(entry.target);
             }
         });
     }, {
+        root: null,
         rootMargin: '0px',
         threshold: 0.1
     });
